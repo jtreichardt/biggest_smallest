c38844737819c8af7ae668e94b073b5856064679
Jared Reichardt <jardreichardt@gmail.com>
2019-03-14 11:50:13 -0500

    moved validation loops below input statements

--- biggest_smallest.py
+++ biggest_smallest.py
@@ -1,24 +1,20 @@
 num1 = float(input('Please input first number (must be between 0 and 10000): '))
+num2 = float(input('Please input second number (must be between 0 and 10000): '))
+num3 = float(input('Please input third number (must be between 0 and 10000): '))
+num4 = float(input('Please input fourth number (must be between 0 and 10000): '))
+num5 = float(input('Please input fifth number (must be between 0 and 10000): '))
 
 while num1 <=0 or num1 >= 10000:
     num1 = float(input('Error, first number must be between 0 and 10000, try again: '))
 
-num2 = float(input('Please input second number (must be between 0 and 10000): '))
-
 while num2 <=0 or num2 >= 10000:
     num2 = float(input('Error, second number must be between 0 and 10000, try again: '))
-
-num3 = float(input('Please input third number (must be between 0 and 10000): '))
 
 while num3 <=0 or num3 >= 10000:
     num3 = float(input('Error, third number must be between 0 and 10000, try again: '))
 
-num4 = float(input('Please input fourth number (must be between 0 and 10000): '))
-
 while num4 <=0 or num4 >= 10000:
     num4 = float(input('Error, fourth number must be between 0 and 10000, try again: '))
-
-num5 = float(input('Please input fifth number (must be between 0 and 10000): '))
 
 while num5 <=0 or num5 >= 10000:
     num5 = float(input('Error, fifth number must be between 0 and 10000, try again: '))
